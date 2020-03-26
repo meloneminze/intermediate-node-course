6b2d13b7558d2b1bb880c78fbc53038ec987d096
meloneminze <melanie@derrabus.de>
2020-03-26 14:29:36 +0100

    Change

--- server.js
+++ server.js
@@ -12,26 +12,55 @@ app.listen(port, () => {
   console.log(`server is listening on port:${port}`);
 });
 
+function sendResponse(res, err, data) {
+  if (err) {
+    res.json({
+      success: false,
+      message: err
+    });
+  } else if (!data) {
+    res.json({
+      success: false,
+      message: "Not Found"
+    });
+  } else {
+    res.json({
+      success: true,
+      data: data
+    });
+  }
+}
+
 // CREATE
 
 app.post("/users", (req, res) => {
-  User.create(
-    {
-      name: req.body.newData.name,
-      email: req.body.newData.email,
-      password: req.body.newData.password
-    },
-    (err, data) => {
-      if (err) {
-        res.json({ success: false, message: err });
-      } else if (!data) {
-        res.json({ success: false, message: "Not Found" });
-      } else {
-        res.json({ success: true, data: data });
-      }
-    }
-  );
+  User.create({ ...req.body.newData }, (err, data) => {
+    sendResponse(res, err, data);
+  });
 });
+
+app
+  .route("/users/:id")
+  .get((req, res) => {
+    User.findById(req.params.id, (err, data) => {
+      sendResponse(res, err, data);
+    });
+  })
+  .put((req, res) => {
+    User.findByIdAndUpdate(
+      req.params.id,
+      { ...req.body.newData },
+      { new: true },
+      (err, data) => {
+        sendResponse(res, err, data);
+      }
+    );
+  })
+  .delete((req, res) => {
+    User.findByIdAndDelete(req.params.id, (err, data) => {
+      sendResponse(res, err, data);
+    });
+  });
 
 app
   .route("/users/:id")
